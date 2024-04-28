Fix rotation bugs, implement agent logic and reproduction/death, implement trail visualization

src/agent.py
@@ -9,22 +9,11 @@ import collections
 def agent_init(key):
     """Generate a random new agent with [dx, dy, counter]."""
 
-    def generate_velocity(carry_in):
-        _, key = carry_in
-        key, subkey = jr.split(key, 2)
-        dx, dy = jr.randint(subkey, shape=(2,), minval=-1, maxval=2)
-        return ((dx,dy),key)
-
-    def is_not_valid(carry_in):
-        ''' Ensures no agents with velocity 0,0 get generated'''
-        dx, dy = carry_in[0]
-        return  (dx + dy) == 0
-
-    init_input = (0,key)
-    carry_out = generate_velocity(init_input)
-
-    velocity, _ = \
-        jax.lax.while_loop(is_not_valid, generate_velocity, carry_out)
+    # use a table because a velocity of 0,0 is not valid, easier to avoid
+    # this way
+    velocities = jnp.array(
+        [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+    velocity = jr.choice(key, velocities)
 
     return jnp.array([*velocity, 0])
 
@@ -37,9 +26,24 @@ def agent_sensor_positions(agent, sensor_length):
     # debbugging (this index should never be reached)
     sensor_map = jnp.array([
         [[(-1,  0), ( 0, -1)], [(-1, -1), ( 1, -1)], [( 0, -1), ( 1,  0)]],
-        [[(-1,  1), (-1, -1)], [( -5,  -5), ( -5,  -5)], [( 1, -1), ( 1,  1)]],
+        [[(-1,  1), (-1, -1)], [( -5,-5), ( -5,-5)], [( 1, -1), ( 1,  1)]],
         [[( 0,  1), (-1,  0)], [( 1,  1), (-1,  1)], [( 1,  0), ( 0,  1)]],
     ])
     y = agent[1] + 1
     x = agent[0] + 1
     return sensor_length * jnp.array(sensor_map[y][x])
+
+@jax.jit
+def agent_sensor_directions(agent):
+    """Compute the directions of the sensors using a sensor map."""
+
+    # velocities with 0,0 are not valid; -5 values make this obvious for 
+    # debbugging (this index should never be reached)
+    sensor_map = jnp.array([
+        [[(-1,  0), ( 0, -1)], [(-1, -1), ( 1, -1)], [( 0, -1), ( 1,  0)]],
+        [[(-1,  1), (-1, -1)], [( -5,-5), ( -5,-5)], [( 1, -1), ( 1,  1)]],
+        [[( 0,  1), (-1,  0)], [( 1,  1), (-1,  1)], [( 1,  0), ( 0,  1)]],
+    ])
+    y = agent[1] + 1
+    x = agent[0] + 1
+    return jnp.array(sensor_map[y][x])

src/main.py
@@ -10,9 +10,9 @@ import pygame
 
 def initialize_config():
     # define all the hyperparameters for the simulation
-    height, width = 100, 100
+    height, width = 200, 200
     upscale = 5
-    initial_population_density = 0.1
+    initial_population_density = 0.0001
 
     trail_deposit = 5
     trail_damping = 0.1
@@ -53,7 +53,9 @@ def draw(scene, config_scene, screen, font, i):
     height, width, upscale, _ = config_scene
 
     # draw the scene
+
     upscaled_shape = upscale * jnp.array([height, width])
+
     surface = pygame.pixelcopy.make_surface(scene_pixelmap(scene, upscaled_shape))
     screen.blit(surface, (0, 0))
 
@@ -64,7 +66,7 @@ def draw(scene, config_scene, screen, font, i):
     pygame.display.update()
 
 
-def run_with_gui(key, num_iter=200):
+def run_with_gui(key, num_iter=500):
     """Run a simulation with gui, this is useful for debugging and getting images."""
     config_scene, config_agent, config_trail, config_chemo = initialize_config()
     height, width, upscale, _ = config_scene
@@ -97,3 +99,4 @@ def run_headless(key):
 if __name__ == '__main__':
     key = jr.PRNGKey(37)
     run_with_gui(key)
+