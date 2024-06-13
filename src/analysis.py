ba2f98c78ccf23621cf34ac1853a8b75a7f7c0e0
borroot <inbox@borroot.com>
2024-06-13 21:24:12 +0200

    Plotsss

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 from main import *
+import matplotlib.pyplot as plt
 import jsonpickle
 import os
 import time
@@ -68,16 +69,93 @@ def show_mst(data):
                 print()
 
 
+def check_enough(results):
+    count = 0
+    for repetition in results:
+        for scene in repetition:
+            if scene.graph().connected:
+                count += 1
+    return count < len(results) * len(results[0]) * 0.7
+
+
+def compute_means(results):
+    cost_means, perf_means, ftol_means = [], [], []
+
+    for s in range(len(results[0])):
+        for r in range(len(results)):
+            scene = results[r][s]
+            graph = scene.graph()
+
+            cost_valid_reps, perf_valid_reps, ftol_valid_reps = [], [], []
+
+            if graph.connected:
+                cost_valid_reps.append(graph.mst_perfect() / graph.cost())
+                perf_valid_reps.append(graph.mst_perfect() / graph.mst_actual())
+                ftol_valid_reps.append(graph.fault_tolerance())
+
+            if len(cost_valid_reps) > 0:
+                cost_means.append(sum(cost_valid_reps) / len(cost_valid_reps))
+                perf_means.append(sum(perf_valid_reps) / len(perf_valid_reps))
+                ftol_means.append(sum(ftol_valid_reps) / len(ftol_valid_reps))
+
+    return np.array(cost_means), np.array(perf_means), np.array(ftol_means)
+
+
+def plot_oneline(means, stds, label, parameter_name, color):
+    plt.fill_between(means.keys(),
+        np.array(list(means.values())) + np.array(list(stds.values())),
+        np.array(list(means.values())) - np.array(list(stds.values())),
+        alpha=0.35, lw=0, color=color
+    )
+
+    plt.scatter(means.keys(), means.values(), color=color)
+    plt.plot(means.keys(), means.values(), color=color, label=label)
+
+    plt.xlabel(parameter_name)
+    plt.xlim([
+        min(map(float, data[parameter_name])),
+        max(map(float, data[parameter_name]))
+    ])
+
+
+def add_means(means_dict, stds_dict, means, parameter_value):
+    means_dict[float(parameter_value)] = np.mean(means)
+    stds_dict[float(parameter_value)] = np.std(means)
+
+
+def plot_all(data):
+    for parameter_name in data:
+        cost_means, cost_stds = {}, {}
+        perf_means, perf_stds = {}, {}
+        ftol_means, ftol_stds = {}, {}
+
+        for parameter_value in data[parameter_name]:
+            print('processing', parameter_name, '=', parameter_value)
+            results = data[parameter_name][parameter_value]
+
+            if check_enough(results):
+                print('not enough data for this batch')
+                continue
+
+            means_cost, means_perf, means_ftol = compute_means(results)
+
+            add_means(cost_means, cost_stds, means_cost, parameter_value)
+            add_means(perf_means, perf_stds, means_perf, parameter_value)
+            add_means(ftol_means, ftol_stds, means_ftol, parameter_value)
+
+        plot_oneline(cost_means, cost_stds, '$1 / \\mathrm{TL_{MRST}}$', parameter_name, 'tab:blue')
+        plot_oneline(perf_means, perf_stds, '$\\mathrm{MD_{MRST}}$', parameter_name, 'tab:orange')
+        plot_oneline(ftol_means, ftol_stds, '$\\mathrm{FT}$', parameter_name, 'tab:green')
+
+        plt.legend()
+        plt.show()
+
+
 if __name__ == '__main__':
-
     # specify here which experiments you want to load in
-    parameter_setups = {
-        # 'initial_population_density': [0.01, 0.04],
-        'reproduction_threshold': [30],
-    }
-    data = load_data('../results', parameter_setups)
-
-    # scenes = data['initial_population_density']['0.04'][0]
-    show_mst(data)
-
-    visualise(data['reproduction_threshold']['30'][2])
+    # parameter_setups = {
+    #     'initial_population_density': [0.01, 0.04, 0.07, .1, .3, .5],
+    #     # 'reproduction_threshold': [30],
+    # }
+    data = load_data('../results')#, parameter_setups)
+    plot_all(data)
